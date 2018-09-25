a87f2869856bd5cb9fa8e1fc9b4acabdfce28a6b
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2018-09-25 12:34:32 +0200

    Added Glew

--- Source/ModuleRenderer3D.cpp
+++ Source/ModuleRenderer3D.cpp
@@ -2,13 +2,14 @@
 #include "Application.h"
 #include "ModuleRenderer3D.h"
 #include "SDL/include/SDL_opengl.h"
+#include "OpenGL.h"
 #include <gl/GL.h>
 #include <gl/GLU.h>
 #include "SDL/include/SDL_version.h"
 
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
-
+#pragma comment(lib, "Glew/libx86/glew32.lib")
 struct SDL_version;
 ModuleRenderer3D::ModuleRenderer3D(bool start_enabled) : Module( start_enabled)
 {
---
+++ Source/OpenGL.h
@@ -0,0 +1,11 @@
+#ifndef __OPENGL__H__
+#define __OPENGL__H__
+
+#define WIN32_MEAN_AND_LEAN
+#include <Windows.h>
+#include "Glew/include/glew.h"
+#include "SDL/include/SDL_opengl.h"
+
+
+
+#endif
