2e16b46f86e1c0f29fe2effc07fe058fe7eab63b
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2018-09-27 17:56:37 +0200

    Mesh class to create all the geometry for next homework

---
+++ Source/Mesh.cpp
@@ -0,0 +1,74 @@
+#include "Mesh.h"
+#include "OpenGL.h"
+
+
+Mesh::Mesh()
+{
+
+}
+Mesh::~Mesh()
+{
+
+}
+
+void Mesh::Draw()
+{
+	//Enable Client
+	glEnableClientState(GL_VERTEX_ARRAY);
+	//Bind Vertices
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_id);
+	glVertexPointer(3, GL_FLOAT, 0, NULL);	//Bind Indices	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_id);
+	//Draw
+	glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, NULL);
+	//Unbind
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//Disable Client
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+void Mesh::DefineCubeVertices()
+{
+
+}
+
+void Mesh::DefinePlaneVertices()
+{
+	//Create Buffers
+	glGenBuffers(1, (GLuint*) &(vertex_id));
+	glGenBuffers(1, (GLuint*) &(indices_id));
+
+	//Define Type
+
+	type = MESH_TYPE::PLANE_M;
+	//Define Vertices
+
+	num_vertices = 4;
+	vertices = new float3[num_vertices];
+
+
+	vertices[0].x = 0.0f;
+	vertices[0].x = 0.0f;
+	vertices[0].x = 0.0f;
+
+	vertices[1].x = 0.0f;
+	vertices[1].x = 0.0f;
+	vertices[1].x = 0.0f;
+
+	vertices[2].x = 0.0f;
+	vertices[2].x = 0.0f;
+	vertices[2].x = 0.0f;
+
+	vertices[3].x = 0.0f;
+	vertices[3].x = 0.0f;
+	vertices[3].x = 0.0f;
+
+	//--------------
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_id);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*num_vertices * 3, vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//Define Indices
+
+}
