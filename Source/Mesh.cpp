3960f16a83891c766456b4267d6e3745025d13ea
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2018-09-29 00:00:11 +0200

    Draw Bug fixing

--- Source/Mesh.cpp
+++ Source/Mesh.cpp
@@ -13,22 +13,7 @@ Mesh::~Mesh()
 
 void Mesh::Draw()
 {
-	if (type == SPHERE_M)
-	{
-		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix();
-		glTranslatef(x, y, z);
 
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_NORMAL_ARRAY);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-		glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
-		glNormalPointer(GL_FLOAT, 0, &normals[0]);
-		glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
-		glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
-		glPopMatrix();
-	}
 	//Enable Client
 	glEnableClientState(GL_VERTEX_ARRAY);
 	//Bind Vertices
