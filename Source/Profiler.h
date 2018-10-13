3d69c905344dd7ac4db030b2157c8833c274f04f
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2018-10-13 18:04:07 +0200

    New Profiler Approach WiP

--- Source/Profiler.h
+++ Source/Profiler.h
@@ -3,7 +3,22 @@
 
 #include "Timer.h"
 #include "ModuleSceneImGui.h"
+class ProfilerUnit {
+public:
+public:
+	std::string name;
+	//init Time
+	int init_time;
+	//Timers
+	Timer PreUpdateTimer;
+	Timer UpdateTimer;
+	Timer PostUpdateTimer;
+	//max time
+	uint UpdatePick;
 
+	std::vector<float> time_buffer;
+
+};
 class Profiler {
 public:
 	Profiler() {};
@@ -17,6 +32,16 @@ public:
 	bool PauseTimer(const char* module_name);
 	bool ResumeTimer(const char* module_name);
 public:
+	ProfilerUnit scene_unit;
+	ProfilerUnit ui_unit;
+
+	ProfilerUnit audio_unit;
+	ProfilerUnit camera_unit;
+	ProfilerUnit input_unit;
+	ProfilerUnit render_unit;
+	ProfilerUnit loader_unit;
+	ProfilerUnit window_unit;
+
 	//timers
 	Timer scene_timer;
 	Timer input_timer;
