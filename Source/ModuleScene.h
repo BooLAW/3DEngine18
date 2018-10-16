b7b55526a5a11f0c672e02fc6033b2bc450b62e4
Josep Pi <josep.pi.serra@gmail.com>
2018-10-16 13:31:21 +0200

    Erasing some comments

--- Source/ModuleScene.h
+++ Source/ModuleScene.h
@@ -17,12 +17,10 @@ public:
 	bool Start();
 	update_status Update(float dt);
 	bool CleanUp();
-	void rapidjsonexamplecode();
 
 	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
 	int RandInt(int min, int max);
 	float RandFloat(float min, float max);
-	//void DisplayRandNumber(bool isActive);
 	void Log(const std::string text);
 	GameObject* CreateNewGameObject();
 	void DrawGameObjects();
@@ -33,7 +31,6 @@ public:
 public:
 	//Scene GameObjects(Now only meshes)
 	std::vector<GameObject*> go_list;
-	//std::vector<Mesh*> go_list;
 	GameObject* scene_root = nullptr;
 	GameObject* imported_go = nullptr;
 	bool has_meshes = false;
