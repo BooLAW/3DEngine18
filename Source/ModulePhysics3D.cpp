421c4f2133981a8e3c580733b2a7754113af7247
Pau Bonet Vall Llebrera <paubv1997@gmail.com>
2018-12-02 20:42:16 +0100

    Added PhysX

--- Source/ModulePhysics3D.cpp
+++ Source/ModulePhysics3D.cpp
@@ -1,17 +1,27 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModulePhysics3D.h"
-#include "Bullet/include//btBulletDynamicsCommon.h"
 
-#ifdef _DEBUG
-#pragma comment (lib, "Bullet/libx86/BulletDynamics_debug.lib")
-#pragma comment (lib, "Bullet/libx86/BulletCollision_debug.lib")
-#pragma comment (lib, "Bullet/libx86/LinearMath_debug.lib")
-#else
-#pragma comment (lib, "Bullet/libx86/BulletDynamics.lib")
-#pragma comment (lib, "Bullet/libx86/BulletCollision.lib")
-#pragma comment (lib, "Bullet/libx86/LinearMath.lib")
-#endif
+
+//#ifdef _DEBUG
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3DEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3CommonDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxFoundationDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3ExtensionsDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3CookingDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxPvdSDKDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxTaskDEBUG_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PsFastXmlDEBUG_x86.lib")
+//#else
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3Common_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxFoundation_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3Extensions_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PhysX3Cooking_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxPvdSDK_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PxTask_x86.lib")
+//#pragma comment (lib, "NvidiaPhysX/Libs/PsFastXml_x86.lib")
+//#endif
 
 
 
@@ -29,8 +39,6 @@ bool ModulePhysics3D::Init()
 {
 	CONSOLE_LOG_INFO("Creating 3D Physics simulation");
 	bool ret = true;
-	InitializeWorld();
-	//CreatePlane();
 	return ret;
 }
 
@@ -44,8 +52,7 @@ void ModulePhysics3D::DrawModuleConfig()
 {
 	if (ImGui::CollapsingHeader("Physics"))
 	{
-		if (ImGui::Button("Test"))
-			bullet_test = true;
+	
 	}
 }
 
@@ -56,15 +63,8 @@ update_status ModulePhysics3D::PreUpdate(float dt)
 
 update_status ModulePhysics3D::Update(float dt)
 {
-	if (bullet_test == true)
-	{
-		BulletTest();
-		bullet_test = false;
-	}
-	for (int i = 0; spheres_list.size() > i; i++)
-	{
-		spheres_list[i];
-	}
+	
+
 	return UPDATE_CONTINUE;
 }
 
@@ -76,27 +76,9 @@ update_status ModulePhysics3D::PostUpdate(float dt)
 bool ModulePhysics3D::CleanUp()
 {
 	CONSOLE_LOG_INFO("Destroying 3D Physics simulation");
-	//delete collision shapes
-		/*for (int j = 0; j < collisionShapes.size(); j++)
-		{
-			btCollisionShape* shape = collisionShapes[j];
-			collisionShapes[j] = 0;
-			delete shape;
-		}*/
 
-	//delete dynamics world
-	delete dynamicsWorld;
-	//delete solver
-	delete constraint_solver;
-	//delete broadphase
-	delete broad_phase;
-	//delete dispatcher
-	delete coll_dispatcher;
 
-	delete coll_config;
 
-	//next line is optional: it will be cleared by the destructor when the array goes out of scope
-	//collisionShapes.clear();
 	return true;
 }
 
@@ -169,150 +151,4 @@ std::list<float2> ModulePhysics3D::GetCubeCollisions()
 	}
 	return collisions_list;
 }
-
-void ModulePhysics3D::InitializeWorld()
-{
-	//create Config
-	coll_config = new btDefaultCollisionConfiguration();
-	//create Dispatcher
-	 coll_dispatcher = new btCollisionDispatcher(coll_config);
-	//create broadphase
-	broad_phase = new btDbvtBroadphase();
-
-	constraint_solver = new btSequentialImpulseConstraintSolver;
-
-	dynamicsWorld = new btDiscreteDynamicsWorld(coll_dispatcher, broad_phase, constraint_solver, coll_config);
-
-	//set Real Gravity for now
-	dynamicsWorld->setGravity(btVector3(0, -10000, 0));
-}
-
-void ModulePhysics3D::CreatePlane()
-{
-	btCollisionShape* colShape = new btStaticPlaneShape(btVector3(0, 1, 0), 0);
-
-		btDefaultMotionState* myMotionState = new btDefaultMotionState();
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(0.0f, myMotionState, colShape);
-
-		btRigidBody* body = new btRigidBody(rbInfo);
-		dynamicsWorld->addRigidBody(body);
-}
-
-void ModulePhysics3D::ShootSphere()
-{
-}
-
-void ModulePhysics3D::BulletTest()
-{
-	btAlignedObjectArray<btCollisionShape*> collisionShapes;
-
-	///create a few basic rigid bodies
-
-	//the ground is a cube of side 100 at position y = -56.
-	//the sphere will hit it at y = -6, with center at -5
-	{
-		btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
-
-		collisionShapes.push_back(groundShape);
-
-		btTransform groundTransform;
-		groundTransform.setIdentity();
-		groundTransform.setOrigin(btVector3(0, -56, 0));
-
-		btScalar mass(0.);
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-		btVector3 localInertia(0, 0, 0);
-		if (isDynamic)
-			groundShape->calculateLocalInertia(mass, localInertia);
-
-		//using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
-		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
-		btRigidBody* body = new btRigidBody(rbInfo);
-
-		//add the body to the dynamics world
-		dynamicsWorld->addRigidBody(body);
-	}
-
-	{
-		//create a dynamic rigidbody
-
-		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
-		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
-		collisionShapes.push_back(colShape);
-
-		/// Create Dynamic Objects
-		btTransform startTransform;
-		startTransform.setIdentity();
-
-
-		btScalar mass(1.f);
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-		btVector3 localInertia(0, -50000, 0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass, localInertia);
-
-		startTransform.setOrigin(btVector3(0, 200, 0));
-
-		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
-		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
-		btRigidBody* body = new btRigidBody(rbInfo);
-
-		dynamicsWorld->addRigidBody(body);
-	}
-
-	/// Do some simulation
-
-	///-----stepsimulation_start-----
-	for (int i = 0; i < 20; i++)
-	{
-		dynamicsWorld->stepSimulation(1.f / 60.f, 10);
-
-		//print positions of all objects
-		for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
-		{
-			btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
-			btRigidBody* body = btRigidBody::upcast(obj);
-			btTransform trans;
-			if (body && body->getMotionState())
-			{
-				body->getMotionState()->getWorldTransform(trans);
-			}
-			else
-			{
-				trans = obj->getWorldTransform();
-			}
-			CONSOLE_LOG_INFO("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
-			if (j != 0 && trans.getOrigin().getY() < -5)//where the box is
-				CONSOLE_LOG_INFO("Box Hitted by body %d \n", j);
-		}
-	}
-
-	///-----stepsimulation_end-----
-
-	//cleanup in the reverse order of creation/initialization
-
-	///-----cleanup_start-----
-
-	//remove the rigidbodies from the dynamics world and delete them
-	for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
-	{
-		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
-		btRigidBody* body = btRigidBody::upcast(obj);
-		if (body && body->getMotionState())
-		{
-			delete body->getMotionState();
-		}
-		dynamicsWorld->removeCollisionObject(obj);
-		delete obj;
-	}
-}
-
 
--- Source/ModulePhysics3D.h
+++ Source/ModulePhysics3D.h
@@ -1,27 +1,13 @@
 #ifndef __MODULE_PHYSICS_3D__
 #define __MODULE_PHYSICS_3D__
-//LINKS ----------------------------ç
-//QuickstartGuide: https://github.com/bulletphysics/bullet3/blob/master/docs/BulletQuickstart.pdf
-//Documentation: https://pybullet.org/wordpress/index.php/forum-2/
-//How Unity Does it: https://unity3d.com/learn/tutorials/s/physics
-	//RigidBody: https://docs.unity3d.com/Manual/RigidbodiesOverview.html
-	//Character Controller: 
-		//https://docs.unity3d.com/Manual/CharacterControllers.html
-		// https://docs.unity3d.com/Manual/class-CharacterController.html
-//----------------------
+
+
 
 #include "Module.h"
 #include "Globals.h"
 #include "Primitive.h"
 
-class btDefaultCollisionConfiguration;
-class btCollisionDispatcher;
-class btBroadphaseInterface;
-class btSequentialImpulseConstraintSolver;
-class btDiscreteDynamicsWorld;
-class btCollisionShape;
-class btDefaultMotionState;
-class btTypedConstraint;
+
 
 class ModulePhysics3D : public Module
 {
@@ -44,12 +30,9 @@ public:
 	//std::list<float2> GetSphereCollisions();
 	std::list<float2> GetCubeCollisions();
 	//-------------------------
+	
 	//Assignment 3--------------
-	void InitializeWorld();
-	void CreatePlane();
-	void ShootSphere();
-	void BulletTest();
-	bool bullet_test = false;
+	
 	//-----------------------
 private:
 	bool debug;
@@ -58,18 +41,7 @@ private:
 	std::vector<AABB> cube_list;
 	std::vector<PSphere> spheres_list;
 
-	//physics
-	btDefaultCollisionConfiguration*	coll_config;
-	btCollisionDispatcher*				coll_dispatcher;
-	btBroadphaseInterface*				broad_phase;
-	btSequentialImpulseConstraintSolver* constraint_solver;
-	btDiscreteDynamicsWorld*			dynamicsWorld;
 	
-
-	std::list<btCollisionShape*> shapes;
-	std::list<PhysBody3D*> bodies;
-	std::list<btDefaultMotionState*> motions;
-	std::list<btTypedConstraint*> constraints;
 };
 #endif
 
